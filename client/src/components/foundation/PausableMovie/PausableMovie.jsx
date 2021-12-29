2cd2bb11628879d1b4d4fd688dd2f34eb9d72072
Kentaro Suzuki <kentaro84207@gmail.com>
2021-12-29 19:50:10 +0900

    feat: convert gif into webm

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -1,10 +1,5 @@
 import classNames from 'classnames';
-import { Animator, Decoder } from 'gifler';
-import { GifReader } from 'omggif';
-import React from 'react';
-
-import { useFetch } from '../../../hooks/use_fetch';
-import { fetchBinary } from '../../../utils/fetchers';
+import React, { useRef } from 'react';
 import { AspectRatioBox } from '../AspectRatioBox';
 import { FontAwesomeIcon } from '../FontAwesomeIcon';
 
@@ -18,61 +13,29 @@ import { FontAwesomeIcon } from '../FontAwesomeIcon';
  * @type {React.VFC<Props>}
  */
 const PausableMovie = ({ src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
+  /**
+   * @type {React.MutableRefObject<HTMLMediaElement>}
+   */
+  const animatorRef = useRef(null);
 
-  /** @type {React.RefObject<import('gifler').Animator>} */
-  const animatorRef = React.useRef(null);
-  /** @type {React.RefCallback<HTMLCanvasElement>} */
-  const canvasCallbackRef = React.useCallback(
-    (el) => {
-      animatorRef.current?.stop();
+  const isReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+  const [isPlaying, setIsPlaying] = React.useState(!isReducedMotion);
 
-      if (el === null || data === null) {
-        return;
-      }
-
-      // GIF を解析する
-      const reader = new GifReader(new Uint8Array(data));
-      const frames = Decoder.decodeFramesSync(reader);
-      const animator = new Animator(reader, frames);
-
-      animator.animateInCanvas(el);
-      animator.onFrame(frames[0]);
-
-      // 視覚効果 off のとき GIF を自動再生しない
-      if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-        setIsPlaying(false);
-        animator.stop();
-      } else {
-        setIsPlaying(true);
-        animator.start();
-      }
-
-      animatorRef.current = animator;
-    },
-    [data],
-  );
-
-  const [isPlaying, setIsPlaying] = React.useState(true);
   const handleClick = React.useCallback(() => {
     setIsPlaying((isPlaying) => {
       if (isPlaying) {
-        animatorRef.current?.stop();
+        animatorRef.current?.pause();
       } else {
-        animatorRef.current?.start();
+        animatorRef.current?.play();
       }
       return !isPlaying;
     });
   }, []);
 
-  if (isLoading || data === null) {
-    return null;
-  }
-
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <canvas ref={canvasCallbackRef} className="w-full" />
+        <video ref={animatorRef} muted autoPlay={!isReducedMotion} src={src} loop playsInline className="w-full" />
         <div
           className={classNames(
             'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
